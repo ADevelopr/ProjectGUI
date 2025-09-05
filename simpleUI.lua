49a4cba531f1e63bb11be84843deb4c762a8189a
TheDumb <fofoangel858@gmail.com>
2025-09-05 17:49:19 -0600

    Update simpleUI.lua

--- simpleUI.lua
+++ simpleUI.lua
@@ -1,1 +1,19 @@
+local UILib = {}
 
+function UILib:Frame()
+    local ScreenGui = Instance.new("ScreenGui")
+    ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+    ScreenGui.IgnoreGuiInset = true
+    ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+    local Frame = Instance.new("Frame")
+    Frame.Parent = ScreenGui
+    Frame.BorderSizePixel = 0
+    Frame.BorderColor3 = Color3.fromRGB(255, 255, 255)
+    Frame.Size = UDim2.new(0, 100, 0, 100)
+    Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+
+    return Frame
+end
+
+return UILib
